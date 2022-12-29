a6a2ece29851ba561233eeba46267c9b33146ff3
Adan Nogueira <adan.nogueira@gmail.com>
2022-12-29 08:55:45 -0300

    feat: add dynamo event

--- handler.py
+++ handler.py
@@ -6,8 +6,11 @@ import os
 import uuid
 import json
 
-s3 = boto3.client('s3')
 size = int(os.environ['THUMBNAIL_SIZE'])
+dbtable = str(os.environ['DYNAMODB_TABLE'])
+region_name = str(os.environ['REGION_NAME'])
+s3 = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb', region_name)
 
 def s3_thumbnail_generator(event, context):
     print('EVENT::', event)
@@ -50,4 +53,24 @@ def upload_to_s3(bucket, key, image, img_size):
     )
     print(response)
     url = f'{s3.meta.endpoint_url}/{bucket}/{key}'
+    s3_save_thumbnail_url_to_dynamo(url_path=url, img_size=img_size)
     return url
+
+def s3_save_thumbnail_url_to_dynamo(url_path, img_size):
+    toint = float(img_size * 0.53)/1000
+    table = dynamodb.Table(dbtable)
+    response = table.put_item(
+        Item={
+            'id': str(uuid.uuid4()),
+            'url': str(url_path),
+            'approxReducedSize': str(toint) + str(' KB'),
+            'createdAt': str(datetime.now()),
+            'updatedAt': str(datetime.now())
+            
+        }
+    )
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(response)
+    }
